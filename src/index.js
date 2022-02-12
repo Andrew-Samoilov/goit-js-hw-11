a1ac7eb830fb8e69f2b301ec6406278a5e700f27
Andrew Samoilov <veprsag@gmail.com>
2022-02-13 00:12:01 +0200

    add imports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './sass/main.scss';
 import * as imageSearch from './js/image-search-app';
+const axios = require('axios').default;
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import imageCardTpl from './templates/image.hbs';
+
+Notiflix.Notify.info(`Hi from Notify.`,);
