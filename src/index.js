2a41fff30f2f65922638ef7a7eb4a5769f2879df
Andrew Samoilov <veprsag@gmail.com>
2022-02-20 17:59:50 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import './sass/main.scss';
+import imgApi from './js/fetch-images';
 import { fetchImages } from './js/fetch-images';
+import { resetPageNumber } from './js/fetch-images';
 const axios = require('axios').default;
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import imageTpl from './templates/image.hbs';
 import getRefs from './js/get-refs';
+// Notiflix.Notify.info(`Hi from Notify.`,);
+
 const refs = getRefs();
+
 refs.btnLoadMore.hidden = true;
-
-Notiflix.Notify.info(`Hi from Notify.`,);
-
 refs.imgForm.addEventListener('submit', onFormSubmit);
-// refs.imgGallery.addEventListener('scroll', onScroll);
+refs.btnLoadMore.addEventListener('click', onLoadMore);
+
 // window.addEventListener('scroll', onScroll);
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    resetPageNumber();
 
     fetchImages(refs.imgField.value)
         .then(renderImages)
-        .catch(onFetchError)
-        .finally(() => Notiflix.Notify.info(`finalize it`));
+        .catch(onFetchError);
 
     refs.btnLoadMore.hidden = false;
 }
@@ -32,21 +35,41 @@ function onFetchError(error) {
 }
 
 function renderImages(images) {
-    console.log(` inside function render images `);
-    console.log(images.hits[0].id);
-    if (!images.total) Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+    console.log(` inside function render images`, images);  // console.log(images.hits[0].id);
+    if (!images.total) {
+        Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+        refs.btnLoadMore.hidden = true;
+        refs.imgGallery.innerHTML = '';
+        return;
+    } else if (images.total > 1) {
+        Notiflix.Notify.info(`Hooray! We found ${images.total} images.`);
+        refs.btnLoadMore.hidden = false;
+    }
 
-    refs.imgGallery.innerHTML = '';
+    if (images.total < 40) {
+        refs.btnLoadMore.hidden = true;
+        Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
+
     const markup = [];
     for (let index = 0; index < images.hits.length; index++) {
         const element = images.hits[index];
         markup.push(imageTpl(element));
     }
 
-    refs.imgGallery.innerHTML = markup.join('');
+    refs.imgGallery.innerHTML += markup.join('');
 }
 
+function onLoadMore() {
+    console.log(` inside function load more `);
+    refs.btnLoadMore.hidden = true;
 
+    fetchImages(refs.imgField.value)
+        .then(renderImages)
+        .catch(onFetchError);
+    // .finally(() => Notiflix.Notify.info(`Hooray! We found ${images.hits[0].id} images.`));
+
+}
 
 // function onScroll() {
 //     // console.log(`inside function onScroll ${window.scrollY}`);
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -3,17 +3,20 @@ let pageGallery = 1;
 export async function fetchImages(imag) {
     const BASE_URL = 'https://pixabay.com/api/';
     const MY_API_KEY = '25686196-d3e700a683e9eec533ed2046f';
-    const options = `image_type=photo&pretty=true&page=${pageGallery}&per_page=40&orientation=horizontal&safesearch=true`;
+    const PER_PAGE = 40;
+    const options = `image_type=photo&pretty=true&page=${pageGallery}&per_page=${PER_PAGE}&orientation=horizontal&safesearch=true`;
     /* https://pixabay.com/api/
     ?key=25686196-d3e700a683e9eec533ed2046f
-    &q=yellow+flowers&image_type=photo&pretty=true
-        const options = 'image_type=photo&pretty=true&page=1&per_page=40&orientation=horizontal&safesearch=true';  */
+    &q=yellow+flowers&image_type=photo&pretty=true */
 
     console.log(` inside function fetchImages ${imag}`);
-    pageGallery++;
-    console.log(`  pageGallery++ ${pageGallery++}`);
+    console.log(`  pageGallery ${pageGallery++}`);
+
     return await fetch(`${BASE_URL}?key=${MY_API_KEY}&q=${imag}&${options}`)
         .then(response => response.json(),
     );
+}
 
+export function resetPageNumber() {
+    pageGallery = 1;
 }
